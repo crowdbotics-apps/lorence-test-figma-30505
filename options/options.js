c76f645273198948fcfbafd7ad10b8d69b018b15
Crowdbotics <team@crowdbotics.com>
2021-09-13 15:42:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lorence_test_figma_30505Identifier",
+  url: "https://lorence_test_figma_30505Identifier.botics.co",
+  api: "https://lorence_test_figma_30505Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
